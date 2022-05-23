30994cbcd53957a4a915cc96489da8bcb4c31daf
Alrappie <alravimahesa@student.ub.ac.id>
2022-05-23 13:52:10 +0700

    FRONT FOR LOGIN,REGISTER,NAVBAR

--- resources/views/User/create.blade.php
+++ resources/views/User/create.blade.php
@@ -6,41 +6,60 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link href="{{asset('css/semua.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('css/create.css')}}" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    @include('Partials.navbar')
-<h2>Register</h2>
-<form method="POST" action="{{ route('regsStore') }}">
-    {{ csrf_field() }}
-    <div class="form-group">
-        <label for="nama">Nama:</label>
-        <input type="text" class="form-control" id="nama" name="nama">
+    {{-- @include('Partials.navbar') --}}
+    <div class="row">
+        <div class="col s12 m12 l6">
+          <div class="masuk_text">
+              <h3>Daftar</h3>
+              <form method="POST" action="{{ route('regsStore') }}">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <input type="text" class="form-control input" id="nama" name="nama" placeholder="Nama" required>
+                </div>
+            
+                <div class="form-group">
+                    <input type="text" class="form-control input" id="nik" name="nik" placeholder="NIK" required>
+                </div>
+            
+                <div class="form-group">
+                    <input type="text" class="form-control input" id="kota_asal" name="kota_asal" placeholder="Kota asal" required>
+                </div>
+            
+                <div class="form-group">
+                    <input type="number" class="form-control input" id="telepon" name="telepon" placeholder="Nomor HP" required>
+                </div>
+            
+                <div class="form-group">
+                    <input type="password" class="form-control input" id="password" name="password" placeholder="Password" required>
+                </div>
+            
+                <div class="form-group">
+                    <button style="cursor:pointer" type="submit" class="tombolsubmit">Submit</button>
+                </div>
+            </form>
+              <p>Sudah memiliki akun? <a href="{{route('sessCreate')}}">Masuk</a></p>
+          </div>
+        </div>
+
+        <div class="col s12 m12 l6">
+          <div class="image_alun">
+              <div class="image_text">
+                  <h3>Butuh Mobil Di Malang?</h3>
+                  <h5>Sewa disini aja!</h5>
+              </div>
+          </div>
+        </div>
     </div>
 
-    <div class="form-group">
-        <label for="nik">NIK:</label>
-        <input type="text" class="form-control" id="nik" name="nik">
-    </div>
-
-    <div class="form-group">
-        <label for="kota_asal">Kota Asal:</label>
-        <input type="text" class="form-control" id="kota_asal" name="kota_asal">
-    </div>
-
-    <div class="form-group">
-        <label for="telepon">Telepon:</label>
-        <input type="text" class="form-control" id="telepon" name="telepon">
-    </div>
-
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-
-    <div class="form-group">
-        <button style="cursor:pointer" type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</form>
-
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </body>
 </html>
--- resources/views/User/login.blade.php
+++ resources/views/User/login.blade.php
@@ -6,26 +6,49 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link href="{{asset('css/login.css')}}" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    @include('Partials.navbar')
-<h2>Log In</h2>
-
-<form method="POST" action="{{route('sessStore')}}">
-    {{ csrf_field() }}
-    <div class="form-group">
-        <label for="telepon">Telepon:</label>
-        <input type="text" class="form-control" id="telepon" name="telepon">
+    {{-- @include('Partials.navbar') --}}
+<div class="row">
+    <div class="col s12 m12 l6">
+      <div class="masuk_text">
+        <h3>Masuk</h3>
+        <form method="POST" action="{{route('sessStore')}}">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <input type="number" class="form-control input" id="telepon" name="telepon" placeholder="Nomor HP" required>
+            </div>
+        
+            <div class="form-group">
+                <input type="password" class="form-control input" id="password" name="password" placeholder="Password" required>
+            </div>
+        
+            <div class="form-group">
+                <button style="cursor:pointer" type="submit" class="browser-default tombolsubmit">Login</button>
+            </div>
+        </form>
+        <p>
+          Belum memiliki akun? <a href="{{route('regsCreate')}}">Daftar</a>
+        </p>
+      </div>
     </div>
-
-    <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" class="form-control" id="password" name="password">
+    <div class="col s12 m12 l6">
+      <div class="image_alun">
+        <div class="image_text">
+          <h3>Butuh Mobil Di Malang?</h3>
+          <h5>Sewa disini aja!</h5>
+        </div>
+      </div>
     </div>
-
-    <div class="form-group">
-        <button style="cursor:pointer" type="submit" class="btn btn-primary">Login</button>
-    </div>
-</form>
+</div>
+  @section('scripts')
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+  @stop
 </body>
 </html>
