1652c5ac7a92aff745301b084058e31b938f36a6
miftahulihsan <vjmiftahulihsan@gmail.com>
2022-05-29 20:12:21 +0700

    navbar pemilik dan navbar penyewa udah selesai gan ;)))

--- resources/views/Partials/navbarPemilik.blade.php
+++ resources/views/Partials/navbarPemilik.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        <link href="{{asset('css/navbarPemilik.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('css/navbar.css')}}" rel="stylesheet" type="text/css" />
         <link href="{{asset('css/semua.css')}}" rel="stylesheet" type="text/css" />
 
         <title>Menu</title>
@@ -16,17 +16,10 @@
                 <span class='logo'>
                 <a class='c' href="{{route('dashboard')}}">Mobil<span class='red'>Malang</span></a>
                 </span>
-                <span style="display: inline-block; margin-left:50px;word-spacing:15px">
-                    <a href="{{route('profView')}}">Profil</a>
-                    <a href="{{route('dashboard')}}">Riwayat</a>
-                    <a>Mobil</a>
-                    <a>Pesanan</a>
-                    <a>Cek ID Pesanan</a>
-                </span>
-                <a  href = "{{route('sessDestroy')}}" class='keluar'>Keluar</a >
+                <a style="margin-left : 65vw;" href = "{{route('sessDestroy')}}" class='keluar'>Keluar</a >
         </nav>
-
-
+        {{-- <a href="{{route('profView')}}">Hi {{ auth()->user()->nama }}</a>
+        <a href="{{route('sessDestroy')}}">Log Out</a> --}}
         @else
                 <a href="{{route('sessCreate')}}">Log In</a>
                 <a href="{{route('regsCreate')}}">Register</a>
