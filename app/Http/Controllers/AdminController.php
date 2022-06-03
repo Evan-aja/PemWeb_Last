c33c5f827cffb6bdf2a13f0af69fe87c20355360
Evan-aja <gebel909@student.ub.ac.id>
2022-06-03 14:13:56 +0700

    buang import

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
-use Session;
 use Exception;
-use Illuminate\Contracts\Session\Session as SessionSession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\ProfileController;
 
 class SessionController extends Controller
